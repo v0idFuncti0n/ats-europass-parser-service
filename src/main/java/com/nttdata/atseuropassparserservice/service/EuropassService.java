3be3006cb26ad2b89a28a977c32026733fa44da8
boukhancha <boukhancha.anass@gmail.com>
2024-02-10 17:00:09 +0100

    extract the xml file from the content of the pdf

--- src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
+++ src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
@@ -11,6 +11,11 @@ import java.nio.charset.StandardCharsets;
 public class EuropassService {
     public String europassToJson(MultipartFile file) throws IOException {
         InputStream inputStream = file.getInputStream();
-        return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        String resultpdf =  IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        String xmlContent = resultpdf.substring(resultpdf.indexOf("<?xml") , resultpdf.indexOf("</Candidate>") + 13 );
+        return  xmlContent;
+
+
+
     }
 }
