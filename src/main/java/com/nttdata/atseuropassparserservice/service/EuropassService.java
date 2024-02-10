fa338d36c9ec9218198c12601ddbd5893f0ab035
Anas <lakchouchanas@gmail.com>
2024-02-10 17:12:33 +0100

    refactor extracting xml from pdf

--- src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
+++ src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
@@ -7,15 +7,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+
 @Service
 public class EuropassService {
     public String europassToJson(MultipartFile file) throws IOException {
-        InputStream inputStream = file.getInputStream();
-        String resultpdf =  IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-        String xmlContent = resultpdf.substring(resultpdf.indexOf("<?xml") , resultpdf.indexOf("</Candidate>") + 13 );
-        return  xmlContent;
-
-
-
+        String pdfRawContent =  IOUtils.toString(file.getInputStream(), StandardCharsets.UTF_8);
+        // Get XML part inside the Europass PDF
+        return pdfRawContent.substring(pdfRawContent.indexOf("<?xml") , pdfRawContent.indexOf("</Candidate>") + 13 );
     }
 }
