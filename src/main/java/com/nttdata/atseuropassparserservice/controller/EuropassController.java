23c5a76c6baeadbb4901a73e5ab0658b6fca9e74
Anas <lakchouchanas@gmail.com>
2024-02-10 15:53:52 +0100

    api to upload a file

--- src/main/java/com/nttdata/atseuropassparserservice/controller/EuropassController.java
+++ src/main/java/com/nttdata/atseuropassparserservice/controller/EuropassController.java
@@ -1,2 +1,32 @@
-package com.nttdata.atseuropassparserservice.controller;public class EuropassController {
+package com.nttdata.atseuropassparserservice.controller;
+
+import com.nttdata.atseuropassparserservice.service.EuropassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class EuropassController {
+
+    private final EuropassService europassService;
+
+    @Autowired
+    public EuropassController(EuropassService europassService) {
+        this.europassService = europassService;
+    }
+
+    @GetMapping(path = "/europass")
+    public ResponseEntity<Object> europassToJson(@RequestParam("file") MultipartFile file) {
+        try {
+            String response = europassService.europassToJson(file);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
+        }
+    }
 }
--- src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
+++ src/main/java/com/nttdata/atseuropassparserservice/service/EuropassService.java
@@ -1,2 +1,14 @@
-package com.nttdata.atseuropassparserservice.service;public class EuropassService {
+package com.nttdata.atseuropassparserservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.*;
+
+@Service
+public class EuropassService {
+    public String europassToJson(MultipartFile file) throws IOException {
+        return null;
+    }
 }
